d0d6323b765b28b04d258138770b7ed3382ae510
Stella <stella.kate.edelmann@gmail.com>
2019-11-08 09:26:03 -0800

    building lists

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import SwList from "./SwList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,6 +14,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <SwList />
     </div>
   );
 }
