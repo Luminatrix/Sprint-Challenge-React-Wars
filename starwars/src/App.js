2b1bdf0ac56c6d5574a02ccc162c1b3e0d8317aa
Stella <stella.kate.edelmann@gmail.com>
2019-11-08 10:06:48 -0800

    debugging, data stopped rendering and api went down, determining if I messed something up or if the data is just not coming through

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,15 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React from "react";
+// import React, {useState, useEffect} from 'react';
 import './App.css';
 import SwList from "./SwList";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+function App() {
 
   return (
     <div className="App">
--- starwars/src/SwList.js
+++ starwars/src/SwList.js
@@ -1,3 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SwCard from "./SwCard"
+import "./App.css";
+import { Container, Row } from "reactstrap";
+
+
+export default function Swcards() {
+    const [card, setCard] = useState([]);
+
+    useEffect(() => {
+        axios
+          .get(`https://swapi.co/api/people/`)
+          .then(res => {
+            const swChar = res.data.results;
+            console.log(swChar);
+            setCard(swChar);
+          })
+          .catch(err => console.log(`ERROR: ${err}`));
+          },[]);   
+
+      return (
+        <Container>
+          <Row>
+            {card.map(char => {
+              return (
+                <SwCard
+                key={char.name}
+                name={char.name}
+                height={char.height}
+                />
+              );
+            })}
+          </Row>
+        </Container>
+      );
+}
